9cf042eb6d149af2652afeba7fd245db76f5d6df
Ranjith Murthy <ranjith.murthy@gmail.com>
2017-02-26 03:07:07 +0100

    added git ,
    added new model and View files

---
+++ LoginNavigation/NewFolder1/TodayPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace LoginNavigation
+{
+	public partial class TodayPage : ContentPage
+	{
+		public TodayPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ LoginNavigation/NewFolder1/UpcomingAppointmentsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LoginNavigation
+{
+	public partial class UpcomingAppointmentsPage : ContentPage
+	{
+		public UpcomingAppointmentsPage ()
+		{
+			InitializeComponent ();
+		}
+
+		async void OnBackButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PopAsync ();
+		}
+	}
+}
+
